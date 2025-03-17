6d3988343c61373ee1c2535200b2203a7d32f65f
kim gisu <kgs9843@naver.com>
2025-03-17 19:59:37 +0900

    feat: prebuild전 세팅

--- app.config.js
+++ app.config.js
@@ -22,6 +22,7 @@ export default {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
+      package: "com.kgs9843.Haruni",
     },
     web: {
       favicon: "./assets/favicon.png",
